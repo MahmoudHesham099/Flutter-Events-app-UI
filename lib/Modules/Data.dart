711546ee0a010353b3ec6b587cab142d77d7628d
hesham <17p6020@eng.asu.edu.eg>
2021-04-01 08:30:55 +0200

    initial + data

--- lib/Modules/Data.dart
+++ lib/Modules/Data.dart
@@ -1,6 +1,8 @@
 import 'package:eventsapp/Modules/Event.dart';
 import 'package:flutter/material.dart';
 
+const Color KAppColor = Color(0xffde554d);
+
 List<Map> categories = [
   {
     "name": 'MUSIC',
@@ -24,6 +26,19 @@ List<Event> weekendEvents = [
   Event(
     name: 'Moon Crush Festival',
     image: 'assets/event1.jpg',
+    date: '24 Dec',
+    category: categories[0],
+    location: 'Barclays Centre',
+    fromTo: '19PM - 22PM',
+    cost: '60-240\$',
+    participants: 1947,
+    duration: 3,
+    about:
+        'Music festivals have come a long way since the psychedelic wig-outs of the \'60s. These days, there\'s a festival for everyone, even if you\'ve shunned most annual mud-fests in the past because you dance to disco and you don\'t like rock. Beyond the traditional London, UK and European festivals, there are fantastic international events from California\'s very Instagram-friendly Coachella to Japan\'s fabulous Fuji Rock, which prides itself on being the \'cleanest festival in the world\'Here are the 50 best music festivals to try before you die.',
+  ),
+  Event(
+    name: 'Marwan Pablo Live at Darb1718',
+    image: 'assets/event0.jpg',
     date: '24 Dec',
     category: categories[0],
     location: 'Barclays Centre',
